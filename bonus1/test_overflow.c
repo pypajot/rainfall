2dfd4ee34978a77e852c8eedd58aa28931bc67c5
ppajot <ppajot@student.42.fr>
2024-01-31 16:18:46 +0100

    bonus1

---
+++ bonus1/test_overflow.c
@@ -0,0 +1,9 @@
+int main()
+{
+        int i;
+        int a;
+        for (i = -1073741810; i > -1073741815 ; i--) {
+                a = 4 * i;
+                printf("i = %i; a = %i\n", i, a);
+        }
+}
